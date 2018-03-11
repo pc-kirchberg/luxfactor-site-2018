f7ea36501ed94903b9937ba6401d5cf7528cddc7
Marks Polakovs <marks@markspolakovs.me>
2018-03-11 20:21:57 +0100

    spying

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'LUXFACTOR 2018',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-70133844-11",
+        head: true,
+      },
+    },
+],
   pathPrefix: "/"
 };
