a80d00638523314123309dc60af26e56ff251d94
Marks Polakovs <marks@markspolakovs.me>
2019-01-25 19:48:02 +0100

    Update entrypoint.sh

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -8,6 +8,8 @@ git config --global user.name "PC Build System"
 
 yarn
 
-yarn run deploy
+node_modules/.bin/gatsby build --prefix-paths
+
+node_modules/.bin/gh-pages -d public -r "https://x-access-token:$GITHUB_TOKEN@github.com/pupcomesl1/luxfactor-site-2018.git"
 
 echo "Done!"
