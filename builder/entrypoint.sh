9c4891d7ae2383b75c3c9de1ca5503e398eefd0d
Marks Polakovs <marks@markspolakovs.me>
2019-01-25 20:09:24 +0100

    Update entrypoint.sh

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Starting deployment"
 
-git config --global user.email "builds@pupilscom-esl1.eu"
+git config --global user.email "github@markspolakovs.me"
 git config --global user.name "PC Build System"
 
 yarn
